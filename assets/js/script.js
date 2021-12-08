3b4e53c1f497435cee0e8cf5c7d5f097c5c55227
Sam Rapowitz <samrapow@gmail.com>
2021-12-07 20:54:11 -0500

    grabbed city input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,18 @@ var requestOptions = {
   };
 var APIKey = '63b4d64dfe4056a23fee52b7d97ce82d';
 var city;
+var submitButton = $('.submit-button');
+var cityInput = $('.city-input');
+
   
   fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey, requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 
+// Wait for submit button to be clicked
+submitButton.on("click", grabCity);
+
+function grabCity() {
+    city = cityInput.val()
+}
