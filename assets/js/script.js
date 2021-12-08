9b42c60ce6a5aa1b705f8fed3c630a59378b280e
Sam Rapowitz <samrapow@gmail.com>
2021-12-07 20:16:10 -0500

    fetched openweather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,11 @@ var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
+var APIKey = '63b4d64dfe4056a23fee52b7d97ce82d';
+var city;
   
-  fetch("api.openweathermap.org/data/2.5/weather?q=atlanta&appid=d91f911bcf2c0f925fb6535547a5ddc9", requestOptions)
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey, requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
+
